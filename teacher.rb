ec7925298cdc850d3b260719cb48b9faf178d9e7
arturo <ahpareja@gmail.com>
2022-07-21 22:07:19 -0500

    Linters fix

--- teacher.rb
+++ teacher.rb
@@ -2,6 +2,7 @@ require './person'
 
 class Teacher < Person
   attr_accessor :specialization
+
   def initialize(age, name, parent_permission, specialization)
     super(age, name, parent_permission)
     @specialization = specialization
