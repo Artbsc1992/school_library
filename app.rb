71709525eaa4fc68a6154708e2f58cf64d13ae88
arturo <ahpareja@gmail.com>
2022-07-26 16:38:59 -0500

    Add rentals to rentals.json

--- app.rb
+++ app.rb
@@ -8,8 +8,8 @@ require 'json'
 class App
   def initialize
     @my_books = File.exist?('./books.json') ? JSON.parse(File.read('./books.json'), create_additions: true) : []
-    @my_rentals = File.exist?('./rentals.json') ? JSON.parse(File.read('rentals.json')) : []
-    @people = File.exist?('./people.json') ? JSON.parse(File.read('people.json'), create_additions:true) : []
+    @my_rentals = File.exist?('./rentals.json') ? JSON.parse(File.read('./rentals.json')) : []
+    @people = File.exist?('./people.json') ? JSON.parse(File.read('./people.json'), create_additions:true) : []
   end
 
   def list_books
--- rental.rb
+++ rental.rb
@@ -18,4 +18,19 @@ class Rental
     @person = person
     person.rentals.push(self) unless person.rentals.include?(self)
   end
+
+  def to_json(*args)
+    {
+      JSON.create_id => self.class.name,
+      'a' => [date, person.id, book.title]
+    }.to_json(*args)
+  end
+
+  def self.json_create(object)
+    {
+      date: object['a'][0],
+      person_id: object['a'][1],
+      book_title: object['a'][2]
+    }
+  end
 end
