557fceed52ca424807ce8f1e2e6678087b00e17f
arturo <ahpareja@gmail.com>
2022-07-26 14:17:05 -0500

    aa

---
+++ storage.rb
@@ -0,0 +1,19 @@
+require './input'
+require './output'
+
+class Storage
+  def initialize
+    @path = './data'
+    @input = Input.new(@path)
+    @output = Output.new(@path)
+  end
+
+  def fetch_data(state)
+    @input.read_books(state)
+  end
+
+  def save_data(state)
+    @output.create_files
+    @output.save_books(state)
+  end
+end
